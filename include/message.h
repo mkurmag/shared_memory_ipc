32bcebbc33d51d50ee53a349b59462a260e9722b
Kurban Mallachiev <mallachiev@ispras.ru>
2023-01-08 01:47:37 +0400

    Minor comment update

--- include/message.h
+++ include/message.h
@@ -2,7 +2,9 @@
 #define _MESSAGE_H_
 
 #include <stdint.h>
-// For simplicity use uint64 value as message
+// For simplicity use uint64 value as message.
+// If there is a need to determine that the message has not been updated since the last read,
+// then some kind of generation is needed.
 struct Message {
     uint64_t val;
     // unsigned length;
